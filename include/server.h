7a3c703afc73d5bf0d57c0827c4c5cafb3ee41fd
Giblert Brandow <simon@0100.se>
2024-03-02 13:15:25 +0100

    Structured repo and updated file header comment, #2

---
+++ include/server.h
@@ -0,0 +1,39 @@
+/**
+ * Author:    Simon Gustafsson (@gilbertbrandow)
+ * Created:   1st of March 2024
+ * 
+ * (©) Copyright MIT License.
+ **/
+
+#ifndef Server_h
+#define Server_h
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+struct Server {
+    int domain;
+    int service; 
+    int protocol; 
+    u_long interface;
+    int port; 
+    int backlog;
+
+    struct sockaddr_in address; 
+    
+    int socket;
+
+    void (*launch)(void);
+};
+
+struct Server server_constructor(
+    int domain, 
+    int service, 
+    int protocol, 
+    u_long interface, 
+    int port, 
+    int backlog, 
+    void(*launch)(void)
+);
+
+#endif
---
+++ source/main.c
@@ -0,0 +1,10 @@
+/**
+ * Author:    Simon Gustafsson (@gilbertbrandow)
+ * Created:   1st of March 2024
+ * 
+ * (©) Copyright MIT License.
+ **/
+
+void main() {
+    
+}
