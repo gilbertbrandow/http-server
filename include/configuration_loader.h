247badb343f5d546bcced419a343230fcbf6c86b
Giblert Brandow <simon@0100.se>
2024-03-06 21:33:47 +0100

    Implemented a function to load configuration and get address_family for server_constructor, #8

---
+++ include/configuration_loader.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_LOADER_H
+#define CONFIG_LOADER_H
+
+void load_configuration_variables(const char *filename);
+
+int get_address_family();
+
+#endif
---
+++ source/configuration_loader.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stddef.h>
+#include <netinet/in.h>
+#include <string.h>
+
+static char address_family[50];
+
+void load_configuration_variables(const char *filename) {
+    
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening config file");
+        fprintf(stderr, "Make sure you have a '%s' file.\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    fscanf(file, "ADDRESS_FAMILY=%49s", address_family);
+
+    fclose(file);
+}
+
+int get_address_family() {
+    if (strcmp(address_family, "AF_INET") == 0) {
+        return AF_INET;
+    } else if (strcmp(address_family, "AF_INET6") == 0) {
+        return AF_INET6;
+    }
+
+    perror("Error reading the ADDRESS_FAMILY variable");
+    fprintf(stderr, "The value '%s' defined as ADDRESS_FAMILY is not a valid value, try AF_INET or AF_INET6.\n", address_family);
+    exit(EXIT_FAILURE);
+}
